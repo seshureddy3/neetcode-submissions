dc12dda420e90a58b6bacbaff56d0bbf3b29359e
seshureddy3 <seshu.kattaiahgari@gmail.com>
2026-05-04 18:48:58 +0530

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let res = "";
+        for(let str of strs) {
+            res += str.length + "#" + str;
+        }
+
+        return res;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        let res = [];
+        let i = 0;
+        while(i < str.length) {
+            let j = i;
+            while(str[j] !== "#") {
+                j++;
+            }
+
+            let len = parseInt(str.substring(i, j));
+            i = j + 1;
+            j = i + len;
+            res.push(str.substring(i, j));
+            i = j;
+        }
+
+        return res;
+    }
+}
